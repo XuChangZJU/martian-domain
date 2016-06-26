27d91ffb07cc5b67d88fa9a69b6015f3613df616
XuChang <chinawraith@163.com>
2016-06-26 21:29:03 +0800

    对validator的输入类型进行了严格化判定

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -5,26 +5,26 @@
 
 'use strict';
 function isMobile(text) {
-    return ( (text ) && ((/^1[3|4|5|7|8]\d{9}$/.test(text))) );
+    return ( (text) && (typeof text === "string") && ((/^1[3|4|5|7|8]\d{9}$/.test(text))) );
 }
 
 function isPassword(text) {
-    return (text && (text.length>=8))
+    return ((text) && (typeof text === "string") && (text.length>=8))
 
 }
 
 function isCaptcha(text) {
-    return (text && (/^\d{4}$/.test(text)))
+    return ((text) && (typeof text === "string") && (/^\d{4}$/.test(text)))
 }
 
 function isIdCardNumber(text) {
-    return (text && (/^\d{6}(18|19|20)?\d{2}(0[1-9]|1[12])(0[1-9]|[12]\d|3[01])\d{3}(\d|X)$/i.test(text)))
+    return ((text)  && (typeof text === "string") && (/^\d{6}(18|19|20)?\d{2}(0[1-9]|1[12])(0[1-9]|[12]\d|3[01])\d{3}(\d|X)$/i.test(text)))
 }
 
 // todo untested
 function isUrl(str) {
 
-    return !!str.match(/(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/g);
+    return ((str) &&  (typeof str === "string") && !!str.match(/(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/g));
 }
 
 module.exports = {
