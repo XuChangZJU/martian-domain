9d94c9cb9e71c4be9330eb7816d7e358f45a1909
XuChang <chinawraith@163.com>
2016-07-05 17:43:49 +0800

    修改了一些常量以及小bug

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -12,12 +12,12 @@ function dateToString(date,fmt){
     };
     if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
     for (var k in o)
-    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
 }
 
 /**计算时间之间的差值，注意date2一定要大于date1
-**/
+ **/
 function dateDiff(scale, date1, date2) {
     if(!(date1 instanceof Date)){
         date1 = new Date(date1);
@@ -28,48 +28,51 @@ function dateDiff(scale, date1, date2) {
     }
     var part = date2.getTime() - date1.getTime(); //相差毫秒
     switch (scale.toLowerCase()) {
-    case "y":
-    return parseInt(date2.getFullYear() - date1.getFullYear());
-    case "m":
-    return parseInt((date2.getFullYear() - date1.getFullYear()) * 12 + (date2.getMonth() - date1.getMonth()));
-    case "d":
-    return parseInt(part / 1000 / 60 / 60 / 24);
-    case "w":
-    return parseInt(part / 1000 / 60 / 60 / 24 / 7);
-    case "h":
-    return parseInt(part / 1000 / 60 / 60);
-    case "n":
-    return parseInt(part / 1000 / 60);
-    case "s":
-    return parseInt(part / 1000);
-    case "l":
-    return parseInt(part);
+        case "y":
+            return parseInt(date2.getFullYear() - date1.getFullYear());
+        case "m":
+            return parseInt((date2.getFullYear() - date1.getFullYear()) * 12 + (date2.getMonth() - date1.getMonth()));
+        case "d":
+            return parseInt(part / 1000 / 60 / 60 / 24);
+        case "w":
+            return parseInt(part / 1000 / 60 / 60 / 24 / 7);
+        case "h":
+            return parseInt(part / 1000 / 60 / 60);
+        case "n":
+            return parseInt(part / 1000 / 60);
+        case "s":
+            return parseInt(part / 1000);
+        case "l":
+            return parseInt(part);
     }
 }
 
 function dateAdd(scale, number, date) {
-  switch (scale.toLowerCase()) {
-  case "y":
-    date.setFullYear(date.getFullYear() + number);
-    break;
-  case "m":
-    date.setMonth(date.getMonth() + number);
-    break;
-  case "d":
-    date.setDate(date.getDate() + number);
-    break;
-  case "h":
-    date.setHours(date.getHours() + number);
-    break;
-  case "n":
-    date.setMinutes(date.getMinutes() + number);
-    break;
-  case "s":
-    date.setSeconds(date.getSeconds() + number);
-    break;
-  default:
-  }
-  return date;
+    if(!(date instanceof Date)){
+        date = new Date(date);
+    }
+    switch (scale.toLowerCase()) {
+        case "y":
+            date.setFullYear(date.getFullYear() + number);
+            break;
+        case "m":
+            date.setMonth(date.getMonth() + number);
+            break;
+        case "d":
+            date.setDate(date.getDate() + number);
+            break;
+        case "h":
+            date.setHours(date.getHours() + number);
+            break;
+        case "n":
+            date.setMinutes(date.getMinutes() + number);
+            break;
+        case "s":
+            date.setSeconds(date.getSeconds() + number);
+            break;
+        default:
+    }
+    return date;
 }
 
 
@@ -82,9 +85,9 @@ function getWeek(date) {
 }
 
 
- module.exports = {
-     dateToString,
-     dateDiff,
-     dateAdd,
-     getWeek
+module.exports = {
+    dateToString,
+    dateDiff,
+    dateAdd,
+    getWeek
 };
