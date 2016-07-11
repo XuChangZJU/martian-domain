40cc7562352873877855fca634814ddd5e36b808
XuChang <chinawraith@163.com>
2016-07-11 08:51:23 +0800

    增加了一种临时钥匙的状态

--- src/constants/keyState.js
+++ src/constants/keyState.js
@@ -6,6 +6,7 @@ module.exports = {
 	stateTempKeyExpiredWithUserConfirm: -3,		// 临时钥匙过期后用户主动确认
 	stateTempKeyExpiredWithoutUserConfirm: -4,	// 临时钥匙过期后用户未主动确认（通过其它途径确认）
 	stateVisible: 0,							// 边界值，小于这个值的钥匙不应该被用户看到
+	stateTempKeyFailToSubmitLog: 4,			//  （客户端ONLY）临时钥匙提交使用日志失败
 	stateLent: 5,								// 主钥匙被借记		(已废弃，目前借记关系通过type来表达)
 	stateWorkable: 10,							// 边界值，小于这个值的钥匙不可以使用
 	stateActive: 11,							// 钥匙可用
