bd30346db2c757b91c505cee561d0a1378009303
lavystord <lavystord@hotmail.com>
2016-07-29 12:57:37 +0800

    add imagePickerOptions

---
+++ src/constants/imagePickerOptions.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Hey on 2016/5/17.
+ */
+
+const options = {
+  title: null,
+  takePhotoButtonTitle: '拍照',
+  chooseFromLibraryButtonTitle: '从手机相册选择',
+  cancelButtonTitle: '取消',
+  quality: 0.5,
+  maxWidth: 300,
+  maxHeight: 300,
+  storageOptions: {
+    skipBackup: true,
+  },
+  allowsEditing: true,
+};
+
+const disableEditOption = {
+  title: null,
+  takePhotoButtonTitle: '拍照',
+  chooseFromLibraryButtonTitle: '从手机相册选择',
+  cancelButtonTitle: '取消',
+  quality: 0.5,
+  maxWidth: 300,
+  maxHeight: 300,
+  storageOptions: {
+    skipBackup: true,
+  },
+  allowsEditing: false,
+};
+
+
+export {
+  options as generalOption,
+  disableEditOption,
+};
