8bf4d4bcaaeae943bf02c33a8408e0d43d3bac74
XuChang <chinawraith@163.com>
2016-08-01 21:13:30 +0800

    更新了imagePickerOption文件缩进

--- src/constants/imagePickerOptions.js
+++ src/constants/imagePickerOptions.js
@@ -3,35 +3,36 @@
  */
 
 const options = {
-  title: null,
-  takePhotoButtonTitle: '拍照',
-  chooseFromLibraryButtonTitle: '从手机相册选择',
-  cancelButtonTitle: '取消',
-  quality: 0.5,
-  maxWidth: 300,
-  maxHeight: 300,
-  storageOptions: {
-    skipBackup: true,
-  },
-  allowsEditing: true,
+	title: null,
+	takePhotoButtonTitle: '拍照',
+	chooseFromLibraryButtonTitle: '从手机相册选择',
+	cancelButtonTitle: '取消',
+	quality: 0.5,
+	maxWidth: 300,
+	maxHeight: 300,
+	storageOptions: {
+		skipBackup: true,
+	},
+	allowsEditing: true,
 };
 
+
 const disableEditOption = {
-  title: null,
-  takePhotoButtonTitle: '拍照',
-  chooseFromLibraryButtonTitle: '从手机相册选择',
-  cancelButtonTitle: '取消',
-  quality: 0.5,
-  maxWidth: 300,
-  maxHeight: 300,
-  storageOptions: {
-    skipBackup: true,
-  },
-  allowsEditing: false,
+	title: null,
+	takePhotoButtonTitle: '拍照',
+	chooseFromLibraryButtonTitle: '从手机相册选择',
+	cancelButtonTitle: '取消',
+	quality: 0.5,
+	maxWidth: 300,
+	maxHeight: 300,
+	storageOptions: {
+		skipBackup: true,
+	},
+	allowsEditing: false,
 };
 
 
 module.exports =  {
-  generalOption: options,
-  disableEditOption,
+	generalOption: options,
+	disableEditOption,
 };
