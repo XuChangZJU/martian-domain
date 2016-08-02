47fd2be37bdc4f6939b1e53277839a756f9aa274
XuChang <chinawraith@163.com>
2016-08-02 14:55:11 +0800

    更新了houseState与leaseState的定义

--- src/constants/leaseState.js
+++ src/constants/leaseState.js
@@ -10,7 +10,7 @@ module.exports = {
 	denied: -5,                 // 被房东拒绝
 	preempted: -3,              // 被别人抢占（抢租下了）
 	completed: -1,            	// 租约完成
-	willBeIgnored: 3,		 	// 将被忽略
+	willBeDenied: 3,		 	// 将被拒绝（房东已经选择了忽略此租客的请求）
 	applying: 10,               // 申请中
 	contracted: 15,			 // 达成意向
 	inContract: 20            	// 生效（执行中）
