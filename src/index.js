15d179b15b7c1bf78d39e153f0c6d3626677391a
XuChang <chinawraith@163.com>
2016-08-04 17:07:18 +0800

    在index中增加了leasePayType和orderState

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 'use strict';
 
 module.exports= {
-    keyState: require("./constants/keyState"),
-    keyType : require("./constants/keyType"),
-    errorCode : require("./constants/errorCode"),
-    bleConstant : require("./constants/bleConstant"),
-    keyActionType : require("./constants/keyActionType"),
-    houseState: require("./constants/houseState"),
-    leaseState: require("./constants/leaseState"),
-    identificationStatus: require("./constants/identificationStatus"),
-    imagePickerOptions: require('./constants/imagePickerOptions'),
-    dateUtils : require("./utils/dateUtils"),
-    stringUtils : require("./utils/stringUtils"),
-    objectUtils : require("./utils/objectUtils"),
-    maskUtils : require("./utils/maskUtils"),
-    netUtils: require("./utils/netUtils"),
-    versionUtils: require("./utils/versionUtils"),
-    pinYinUtils: require("./utils/pinYinUtils2"),
-    validator: require("./validator/validator")
+	keyState: require("./constants/keyState"),
+	keyType : require("./constants/keyType"),
+	errorCode : require("./constants/errorCode"),
+	bleConstant : require("./constants/bleConstant"),
+	keyActionType : require("./constants/keyActionType"),
+	houseState: require("./constants/houseState"),
+	leaseState: require("./constants/leaseState"),
+	leasePayType: require("./constants/leasePayType"),
+	orderState: require("./constants/orderState"),
+	identificationStatus: require("./constants/identificationStatus"),
+	imagePickerOptions: require('./constants/imagePickerOptions'),
+
+	dateUtils : require("./utils/dateUtils"),
+	stringUtils : require("./utils/stringUtils"),
+	objectUtils : require("./utils/objectUtils"),
+	maskUtils : require("./utils/maskUtils"),
+	netUtils: require("./utils/netUtils"),
+	versionUtils: require("./utils/versionUtils"),
+	pinYinUtils: require("./utils/pinYinUtils2"),
+	validator: require("./validator/validator")
 };
