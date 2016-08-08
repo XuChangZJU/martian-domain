10c60abe9a986566d7fb353205d9c060808b3323
XuChang <chinawraith@163.com>
2016-08-08 17:29:00 +0800

    更改了houseDecoder

--- src/constants/houseStateDecoder.js
+++ src/constants/houseStateDecoder.js
@@ -8,12 +8,12 @@ const decoder = {
 	[houseState.denied]: "被拒绝",
 	[houseState.inVerifying]: "审核中",
 	[houseState.offline]: "已下线",
-	[houseState.willFree]: "即将到期",
+	[houseState.willFree]: "将到期",
 	[houseState.free]: "可租赁",
-	[houseState.contracted]: "已达成意向",
-	[houseState.ordered]: "已预订",
+	[houseState.contracted]: "已签约",
+	[houseState.ordered]: "已租下",
 	[houseState.rented]: "有租约",
-	[houseState.willOffline]: "到期后下线"
+	[houseState.willOffline]: "到期下线"
 };
 
 function decode(state) {
