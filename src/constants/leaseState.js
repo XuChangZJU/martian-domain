bbdd6537b12d9f9fd061344ff3d4eba0512377cd
XuChang <chinawraith@163.com>
2016-08-24 15:48:50 +0800

    更新了leaseState的定义，以便排序

--- src/constants/leaseState.js
+++ src/constants/leaseState.js
@@ -4,18 +4,18 @@
 "use strict";
 
 module.exports = {
-	aborted: -99,      		// 提前中止
+	completed: -99,            	// 租约完成
 	//abortedByLord: -48,		 // 提前中止（房东）
-	willAborted: -15,			// 将要提前中止
 	ignored: -12,				 // 忽略（房东不同意租给此租客）
 	outDated: -10,               // 过期（一定时间后无响应）
 	cancelled: -8,				// 已取消
 	denied: -5,                 // 被房东拒绝
+	aborted: -4,      			// 提前中止
 	preempted: -3,              // 被别人抢占（抢租下了）
-	completed: -1,            	// 租约完成
+	willAborted: -2,			// 将要提前中止
 	willBeDenied: 3,		 	// 将被拒绝（房东已经选择了忽略此租客的请求）
 	applying: 10,               // 申请中
 	contracted: 15,			 // 达成意向
+	willCompleted: 19,		 // 即将到期
 	implementation: 20,        // 生效（执行中）
-	willCompleted: 99,		 // 即将到期
 };
