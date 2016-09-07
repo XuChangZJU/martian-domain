52bf237713e5b6ab8730b34fae118143e14423a6
XuChang <chinawraith@163.com>
2016-09-07 11:14:38 +0800

    更新了看房的状态定义

--- src/constants/lookState.js
+++ src/constants/lookState.js
@@ -11,9 +11,9 @@ module.exports = {
 	invalid2: -20,			// 被抢租（已付款尚未看房）
 	invalid1: -10,			// 被抢租（未付款）
 	init: 1,				// 初始差太
-	ordered: 3,			// 已创建订单未支付
 	avail1: 5,				// 可用状态（使用临时钥匙看房）
 	avail2: 8,				// 可用状态（上任租客在租）
 	looking: 18,			// 正在看房
-	end: 30					// 已看过
+	end: 3,					// 已看过
+	outDated: 30,			// 超时未确认离开
 };
