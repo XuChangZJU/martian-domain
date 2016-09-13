8b0837d5ac202dc6f6e457f40bf975a2482fb412
lavystord <lavystord@hotmail.com>
2016-09-13 16:53:06 +0800

    add new option to imagePickerOption

--- src/constants/imagePickerOptions.js
+++ src/constants/imagePickerOptions.js
@@ -31,8 +31,40 @@ const disableEditOption = {
 	allowsEditing: false,
 };
 
+const hdOption = {
+	title: null,
+	takePhotoButtonTitle: '拍照',
+	chooseFromLibraryButtonTitle: '从手机相册选择',
+	cancelButtonTitle: '取消',
+	quality: 0.95,
+	maxWidth: 1600, // photos only
+	maxHeight: 1600, // photos only
+	allowsEditing: false, // Built in functionality to resize/reposition the image after selection
+	storageOptions: { // if this key is provided, the image will get saved in the documents directory on ios, and the pictures directory on android (rather than a temporary directory)
+		skipBackup: true, // ios only - image will NOT be backed up to icloud
+		path: 'images' // ios only - will save image at /Documents/images rather than the root
+	}
+};
+
+const hdEditableOption = {
+	title: null,
+	takePhotoButtonTitle: '拍照',
+	chooseFromLibraryButtonTitle: '从手机相册选择',
+	cancelButtonTitle: '取消',
+	quality: 0.95,
+	maxWidth: 1600, // photos only
+	maxHeight: 1600, // photos only
+	allowsEditing: false, // Built in functionality to resize/reposition the image after selection
+	storageOptions: { // if this key is provided, the image will get saved in the documents directory on ios, and the pictures directory on android (rather than a temporary directory)
+		skipBackup: true, // ios only - image will NOT be backed up to icloud
+		path: 'images' // ios only - will save image at /Documents/images rather than the root
+	}
+};
+
 
 module.exports =  {
 	generalOption: options,
 	disableEditOption,
+	hdOption,
+	hdEditableOption,
 };
