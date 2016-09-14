d31462d274354b6f63720fb4aa462888153334e5
lavystord <lavystord@gmail.com>
2016-09-14 15:09:27 +0800

    fix imagePickerOption bug

--- src/constants/imagePickerOptions.js
+++ src/constants/imagePickerOptions.js
@@ -54,7 +54,7 @@ const hdEditableOption = {
 	quality: 0.95,
 	maxWidth: 1600, // photos only
 	maxHeight: 1600, // photos only
-	allowsEditing: false, // Built in functionality to resize/reposition the image after selection
+	allowsEditing: true, // Built in functionality to resize/reposition the image after selection
 	storageOptions: { // if this key is provided, the image will get saved in the documents directory on ios, and the pictures directory on android (rather than a temporary directory)
 		skipBackup: true, // ios only - image will NOT be backed up to icloud
 		path: 'images' // ios only - will save image at /Documents/images rather than the root
