1c16cdc2f08a5b53c58f997679428ab2d5f968ae
XuChang <chinawraith@163.com>
2016-09-26 17:27:19 +0800

    增加了消息的状态states

--- src/constants/messageConstant.js
+++ src/constants/messageConstant.js
@@ -3,54 +3,61 @@
  */
 "use strict";
 const weights = {
-		tiny: 10,
-		low: 10,
-		medium: 20,
-		high: 60,
-		top: 90
-	};
+    tiny: 10,
+    low: 10,
+    medium: 20,
+    high: 60,
+    top: 90
+};
 
 const icons = {
-	warning: {
-		group: "core",
-		name: "wrong"
-	},
-	money: {
-		group: "core",
-		name: "mobile"
-	},
-	deny: {
-		group: "core",
-		name: "mobile"
-	},
-	fail: {
-		group: "core",
-		name: "wrong"
-	},
-	happy: {
-		group: "core",
-		name: "wrong"
-	}
+    warning: {
+        group: "core",
+        name: "wrong"
+    },
+    money: {
+        group: "core",
+        name: "mobile"
+    },
+    deny: {
+        group: "core",
+        name: "mobile"
+    },
+    fail: {
+        group: "core",
+        name: "wrong"
+    },
+    happy: {
+        group: "core",
+        name: "wrong"
+    }
 };
 const words = {
-	share: "享",
-	identify: "验",
-	pass: "过",
-	key: "钥",
-	warning: "警",
-	receive: "收",
-	pay: "付",
-	rent: "租",
-	denied: "拒",
-	fail: "失",
-	violate: "违",
-	push: "催",
-	online: "上",
-	return :"退"
+    share: "享",
+    identify: "验",
+    pass: "过",
+    key: "钥",
+    warning: "警",
+    receive: "收",
+    pay: "付",
+    rent: "租",
+    denied: "拒",
+    fail: "失",
+    violate: "违",
+    push: "催",
+    online: "上",
+    return :"退"
+};
+const states = {
+    init: 0,
+    sending: 1,
+    success: 2,
+    failure: 3
 };
 
 module.exports = {
-	icons,
-	words,
-	weights,
+    icons,
+    words,
+    weights,
+    states
 }
