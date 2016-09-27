3645ef2ea4c81ee18c10a057c58b072ab9e84a72
XuChang <chinawraith@163.com>
2016-09-27 13:18:17 +0800

    增加了房屋下线的lookState

--- src/constants/lookState.js
+++ src/constants/lookState.js
@@ -4,16 +4,17 @@
 "use strict";
 
 module.exports = {
-	invalid6: -60,			// 钥匙过期
-	invalid5: -50,			// 主动取消
-	invalid4: -40,			// 自己租下
-	invalid3: -30,			// 其他租客抢租下
-	invalid2: -20,			// 被抢租（已付款尚未看房）
-	invalid1: -10,			// 被抢租（未付款）
-	init: 1,				// 初始差太
-	avail1: 5,				// 可用状态（使用临时钥匙看房）
-	avail2: 8,				// 可用状态（上任租客在租）
-	looking: 18,			// 正在看房
-	end: 3,					// 已看过
-	outDated: 30,			// 超时未确认离开
+    invalid7: -70,			// 房屋下线
+    invalid6: -60,			// 钥匙过期
+    invalid5: -50,			// 主动取消
+    invalid4: -40,			// 自己租下
+    invalid3: -30,			// 其他租客抢租下
+    invalid2: -20,			// 被抢租（已付款尚未看房）
+    invalid1: -10,			// 被抢租（未付款）
+    init: 1,				// 初始差太
+    avail1: 5,				// 可用状态（使用临时钥匙看房）
+    avail2: 8,				// 可用状态（上任租客在租）
+    looking: 18,			// 正在看房
+    end: 3,					// 已看过
+    outDated: 30,			// 超时未确认离开
 };
