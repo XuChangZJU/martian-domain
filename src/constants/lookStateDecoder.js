a64bfe2b969e5ff23d55d5466f642c8512653c90
XuChang <chinawraith@163.com>
2016-10-28 15:01:01 +0800

    更新了lookState的状态

--- src/constants/lookStateDecoder.js
+++ src/constants/lookStateDecoder.js
@@ -9,14 +9,16 @@ const stateDecoder = {
 	[LookState.invalid2]: "已出租",
 	[LookState.invalid3]: "已出租",
 	[LookState.invalid4]: "已出租",
-	[LookState.invalid5]: "已取消",
-	[LookState.invalid6]: "已过期",
+	[LookState.invalid5]: "已出租",
+	[LookState.invalid6]: "已出租",
 	[LookState.init]: "",
-	[LookState.ordered]: "待支付",
+	[LookState.cancel1]: "已取消",
+    [LookState.cancel2]: "已取消",
 	[LookState.avail1]: "看房路上",
 	[LookState.avail2]: "联系租客",
 	[LookState.looking]: "看房中",
-	[LookState.end]: "已看过"
+	[LookState.end]: "已看过",
+    [LookState.outDated]: "过期待确认"
 };
 
 function decode(state) {
