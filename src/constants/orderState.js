bcd6c6e2fed16be9d1aa74453fb08bd326511883
XuChang <chinawraith@163.com>
2016-11-01 17:39:13 +0800

    修改了订单的相应状态

--- src/constants/orderState.js
+++ src/constants/orderState.js
@@ -8,6 +8,7 @@ module.exports = {
     paying: 3,
     partialPayed: 5,
     paid: 10,
+    returning: 20,
     failed: 51,
     closed1: 101,       // 支付成功且关闭
     closed2: 105,       // 退款成功且关闭
--- src/constants/payConstant.js
+++ src/constants/payConstant.js
@@ -6,18 +6,12 @@ const state = {
     paying: 1,
     paid: 10,
     failed: 51,
-    closed1: 101,       // 支付成功且关闭
-    closed2: 105,       // 退款成功且关闭
-    closed3: 109,       // 支付失败且关闭
 };
 
 const stateDecoder = {
     [state.paying]: "正在支付",
     [state.paid]: "支付完成",
     [state.failed]: "支付失败",
-    [state.closed1]: "已关闭（支付成功）",
-    [state.closed2]: "已关闭（退款成功）",
-    [state.closed3]: "已关闭（支付失败）",
 }
 
 function decodeState(s) {
