ab37bfb29d284c0f24d8865f6a92c8e06bc7f5fe
XuChang <chinawraith@163.com>
2016-11-08 17:21:05 +0800

    增加了orderType

---
+++ src/constants/orderType.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/11/8.
+ */
+module.exports = {
+    rent: 1,        // 租金
+    deposit: 2,     // 押金
+    looking: 3,     // 看房金
+    compensate: 4,   // 违约金
+    tax: 5,         // 平台税金
+};
--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ module.exports= {
     idStatus2Decoder2: require("./constants/idStatus2Decoder2"),
     payConstant: require("./constants/payConstant"),
     withDrawConstant: require("./constants/withDrawConstant"),
+    orderType: require("./constants/orderType"),
 
     dateUtils : require("./utils/dateUtils"),
     stringUtils : require("./utils/stringUtils"),
