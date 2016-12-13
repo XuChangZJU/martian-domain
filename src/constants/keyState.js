ded62300995577204f009df1a8be47261c15b553
XuChang <chinawraith@163.com>
2016-12-13 13:18:04 +0800

    提交了一种新的钥匙状态disabled

--- src/constants/keyState.js
+++ src/constants/keyState.js
@@ -6,8 +6,9 @@ module.exports = {
 	stateTempKeyExpiredWithUserConfirm: -3,		// 临时钥匙过期后用户主动确认
 	stateTempKeyExpiredWithoutUserConfirm: -4,	// 临时钥匙过期后用户未主动确认（通过其它途径确认）
 	stateVisible: 0,							// 边界值，小于这个值的钥匙不应该被用户看到
+	stateDisabled: 2,							// 主钥匙被禁用
 	stateTempKeyFailToSubmitLog: 4,			//  （客户端ONLY）临时钥匙提交使用日志失败
-	stateLent: 5,								// 主钥匙被借记		(已废弃，目前借记关系通过type来表达)
+	stateLent: 5,								// 主钥匙被借记
 	stateWorkable: 10,							// 边界值，小于这个值的钥匙不可以使用
 	stateActive: 11,							// 钥匙可用
 	stateTempKeyUsed: 12,						// 临时钥匙被使用,确认离开前
