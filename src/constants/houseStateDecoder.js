4961966d0bd620f06dc1b2cb9645fcb36784e030
XuChang <chinawraith@163.com>
2017-01-17 17:05:34 +0800

    补充了一个Decoder

--- src/constants/houseStateDecoder.js
+++ src/constants/houseStateDecoder.js
@@ -10,6 +10,7 @@ const decoder = {
 	[houseState.inVerifying]: "审核中",
 	[houseState.offline]: "已下线",
 	[houseState.willFree]: "将到期",
+	[houseState.shadow]: "租约未激活",
 	[houseState.free]: "可租赁",
 	[houseState.ordered]: "已预订",
 	[houseState.rented]: "有租约",
