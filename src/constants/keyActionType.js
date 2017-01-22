02bb5fa9ada45325e5a5bca02c5d2d8e3c3cec5b
XuChang <chinawraith@163.com>
2017-01-22 18:11:14 +0800

    增加了一种keyActionType

--- src/constants/keyActionType.js
+++ src/constants/keyActionType.js
@@ -1,9 +1,10 @@
-'use strict'
+'use strict';
 
 module.exports = {
-	unlock: 1,
-	confirm: 2,
-	dfu: 3,
-	getKeyWord: 4,
-	confirmOutdate: 5
-}
+    unlock: 1,
+    confirm: 2,
+    dfu: 3,
+    getKeyWord: 4,
+    confirmOutdate: 5,
+    clearBonds: 6,
+};
