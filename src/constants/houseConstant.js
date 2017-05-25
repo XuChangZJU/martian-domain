48e925162b43d016ec9556a77fa9b6a4466db548
XuChang <chinawraith@163.com>
2017-05-25 15:40:16 +0800

    增加了houseConstant

---
+++ src/constants/houseConstant.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Administrator on 2017/5/25.
+ */
+const category = {
+    house: 1,
+    apartment: 3,
+    hotel: 5,
+    spec: 99,
+    room: 199,
+};
+
+const decodeCategory = (c) => {
+    const CATEGORY_MATRIX = {
+        [category.house]: '房屋',
+        [category.apartment]: '公寓',
+        [category.hotel]: '酒店',
+        [category.spec]: '规格',
+        [category.room]: '房间',
+    };
+
+    return CATEGORY_MATRIX[c];
+};
+
+const state = require('./houseState');
+const decodeHouse = require('./houseStateDecoder').decode;
+const decodeHouse2 = require('./houseStateDecoder2').decode;
+
+module.exports = {
+    state,
+    category,
+    decodeCategory,
+    decodeHouse,
+    decodeHouse2,
+};
