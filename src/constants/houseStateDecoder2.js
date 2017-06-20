c2607c8aeae170916951ca3fd42f4254a43f9565
XuChang <chinawraith@163.com>
2017-06-20 21:55:51 +0800

    更新了decoder文字

--- src/constants/houseStateDecoder2.js
+++ src/constants/houseStateDecoder2.js
@@ -12,7 +12,7 @@ const decoder = {
 	[houseState.willFree]: "将到期",
 	[houseState.shadow]: "租约未激活",
 	[houseState.free]: "可租赁",
-	[houseState.online]: "线上",
+	[houseState.online]: "通过",
 	[houseState.ordered]: "已预订",
 	[houseState.rented]: "出租中",
 	[houseState.willOffline]: "到期下线"
