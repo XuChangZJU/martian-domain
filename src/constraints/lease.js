27122ff005de5438b64db914aab4d853ff911cb4
wangyuef <929077462@qq.com>
2017-07-03 14:16:05 +0800

    修正几个错误

--- src/constraints/lease.js
+++ src/constraints/lease.js
@@ -15,9 +15,9 @@ const ableToEndLease = (data)=> {
     const {id} = data.house || {};
     const houseState = (data.house || {}).state;
     const userId = data.userId;
-    const options = LeaseUnits[unit];
-    const availableLine = DateUtils.dateAdd(options.precision, -options.volumesForBeToOff, willEndsAt);
-    if (LeaseState.willCompleted === leaseState && houseState === HouseState.willFree && beginsAt > Date.now() && deadPoint === willEndsAt && availableLine <= Date.now() && lordId === userId) {
+    const options = LeaseUnits.units[unit];
+    const availableLine = DateUtils.dateAdd(options.precision, -options.volumesForBeToOff, willEndsAt).valueOf();
+    if (LeaseState.willCompleted === leaseState && houseState === HouseState.willFree && beginsAt < Date.now() && deadPoint === willEndsAt && availableLine <= Date.now() && lordId === userId) {
         return true;
     }
     return false;
@@ -29,9 +29,9 @@ const ableToOffLease = (data)=> {
     const {id} = data.house || {};
     const houseState = (data.house || {}).state;
     const userId = data.userId;
-    const options = LeaseUnits[unit];
-    const unAvailableLine = DateUtils.dateAdd(options.precision, -options.volumesForBeToOff, willEndsAt);
-    if (beginsAt > Date.now() && leaseState === LeaseState.implementation && houseState === HouseState.rented && unAvailableLine > Date.now() && [lordId, tenantId].includes(userId)) {
+    const options = LeaseUnits.units[unit];
+    const unAvailableLine = DateUtils.dateAdd(options.precision, -options.volumesForBeToOff, willEndsAt).valueOf();
+    if (beginsAt < Date.now() && leaseState === LeaseState.implementation && houseState === HouseState.rented && unAvailableLine > Date.now() && [lordId, tenantId].includes(userId)) {
         return true;
     }
     return false;
@@ -43,9 +43,9 @@ const ableToReLease = (data)=> {
     const {id} = data.house || {};
     const houseState = (data.house || {}).state;
     const userId = data.userId;
-    const options = LeaseUnits[unit];
-    const availableLine = DateUtils.dateAdd(options.precision, -options.volumesForBeToOff, willEndsAt);
-    if (beginsAt > Date.now() && leaseState === LeaseState.willCompleted && houseState === HouseState.willFree && availableLine <= Date.now() && tenantId === userId && deadPoint === willEndsAt) {
+    const options = LeaseUnits.units[unit];
+    const availableLine = DateUtils.dateAdd(options.precision, -options.volumesForBeToOff, willEndsAt).valueOf();
+    if (beginsAt < Date.now() && leaseState === LeaseState.willCompleted && houseState === HouseState.willFree && availableLine <= Date.now() && tenantId === userId && deadPoint === willEndsAt) {
         return true;
     }
     return false;
