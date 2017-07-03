a7d18f8df793342cecc89e77cf82c225aaa97316
wangyuef <929077462@qq.com>
2017-07-03 10:53:42 +0800

    增加几个lease的contrainer

--- src/constraints/lease.js
+++ src/constraints/lease.js
@@ -1,22 +1,58 @@
+'use strict';
+
 /**
  * Created by Administrator on 2017/6/26.
  */
-const HouseState = require('../constants/houseState');
+var HouseState = require('../constants/houseState');
+var LeaseState = require('../constants/leaseConstant').state;
+const DateUtils = require("martian-domain").dateUtils;
 
-const extensible = (data) => {
-    const { lease, house, userId } = data;
+const LeaseUnits = require("../constants/leaseConstant").LeaseUnits;
 
-    if (lease.tenantId === userId) {
-        const now = Date.now();
-
-        if ([HouseState.rented, HouseState.willFree].includes(house.state)
-            && lease.willEndsAt - now < 30 * 24 * 3600 * 1000) {
-            return true;
-        }
+const ableToEndLease = (data)=> {
+    const {willEndsAt, beginsAt, deadPoint, unit, lordId} = data.lease || {};
+    const leaseState = (data.lease || {}).state;
+    const {id} = data.house || {};
+    const houseState = (data.house || {}).state;
+    const userId = data.userId;
+    const options = LeaseUnits[unit];
+    const availableLine = DateUtils.dateAdd(options.precision, -options.volumesForBeToOff, willEndsAt);
+    if (LeaseState.willCompleted === leaseState && houseState === HouseState.willFree && beginsAt > Date.now() && deadPoint === willEndsAt && availableLine <= Date.now() && lordId === userId) {
+        return true;
     }
     return false;
-};
+}
+
+const ableToOffLease = (data)=> {
+    const {willEndsAt, beginsAt, deadPoint, unit, lordId, tenantId} = data.lease || {};
+    const leaseState = (data.lease || {}).state;
+    const {id} = data.house || {};
+    const houseState = (data.house || {}).state;
+    const userId = data.userId;
+    const options = LeaseUnits[unit];
+    const unAvailableLine = DateUtils.dateAdd(options.precision, -options.volumesForBeToOff, willEndsAt);
+    if (beginsAt > Date.now() && leaseState === LeaseState.implementation && houseState === HouseState.rented && unAvailableLine > Date.now() && [lordId, tenantId].includes(userId)) {
+        return true;
+    }
+    return false;
+}
+
+const ableToReLease = (data)=> {
+    const {willEndsAt, beginsAt, deadPoint, unit, lordId, tenantId} = data.lease || {};
+    const leaseState = (data.lease || {}).state;
+    const {id} = data.house || {};
+    const houseState = (data.house || {}).state;
+    const userId = data.userId;
+    const options = LeaseUnits[unit];
+    const availableLine = DateUtils.dateAdd(options.precision, -options.volumesForBeToOff, willEndsAt);
+    if (beginsAt > Date.now() && leaseState === LeaseState.willCompleted && houseState === HouseState.willFree && availableLine <= Date.now() && tenantId === userId && deadPoint === willEndsAt) {
+        return true;
+    }
+    return false;
+}
 
 module.exports = {
-    extensible,
+    ableToEndLease,
+    ableToOffLease,
+    ableToReLease
 };
