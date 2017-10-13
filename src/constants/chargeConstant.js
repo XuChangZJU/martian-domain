deb2eb7473448df0bb1e758664a801b731c061ff
wangyuef <929077462@qq.com>
2017-10-13 16:56:10 +0800

    增加la相关常量

--- src/constants/chargeConstant.js
+++ src/constants/chargeConstant.js
@@ -8,12 +8,14 @@ const method = {
     byOrganization: 1,
     byLord: 2,
     none: 255,
+    martianLa: 300
 };
 
 const methodDecoder = {
     [method.byOrganization]: "租客自主交纳",
     [method.byLord]: "由房东收取",
     [method.none]: "无",
+    [method.martianLa]: "智能电表",
 }
 
 function decodeMethod(m) {
