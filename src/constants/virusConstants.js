e03ceb0f281699e1de10c8209db2236928fcc0cb
wangyuef <929077462@qq.com>
2017-12-08 15:41:43 +0800

    增加virusConstants常量

--- src/constants/virusConstants.js
+++ src/constants/virusConstants.js
@@ -2,16 +2,18 @@
  * Created by Administrator on 2016/11/1.
  */
 "use strict";
+const LeaseConstants = require("./leaseConstant");
 const VirusTypes = [
     {
         id: 1,
         name: "房东的私心",
-        describe: "房东发出，被感染者无法看到一定范围内比当前房屋更便宜的房子"
+        describe: "房东对自己的租客发出，被感染者无法看到一定范围内比当前房屋更便宜的房子",
+        leaseStateMtx: [LeaseConstants.state.shadow, LeaseConstants.state.waitingForHouseOnline, LeaseConstants.state.incomplete]
     },
     {
         id: 2,
         name: "屌丝的自救",
-        describe:"屏蔽指定房东"
+        describe: "屏蔽指定房东"
     }
 ];
 
