66d1fc50847f60a5dd464a24113af47f0217681c
wangyuef <929077462@qq.com>
2017-12-20 14:12:31 +0800

    增加roomMateConstant常量

--- src/constants/roomMateConstant.js
+++ src/constants/roomMateConstant.js
@@ -3,11 +3,15 @@
  */
 const mateType = {
     roomMate: 1,
+    administrator: 2,
+    tempWorker: 3,
 };
 
 const decodeCategory = (c) => {
     const STRING_OF_CATEGORY = {
         [mateType.roomMate]: '室友',
+        [mateType.administrator]: '管理员',
+        [mateType.tempWorker]: '临时工作人员'
     };
 
     return STRING_OF_CATEGORY[c];
