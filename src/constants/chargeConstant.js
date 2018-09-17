a7958cdaf94cb3b9fee9078a99f4e4bba88c8430
XuChang <chinawraith@163.com>
2018-09-17 13:54:09 +0800

    将chargeType搬上来

--- src/constants/chargeConstant.js
+++ src/constants/chargeConstant.js
@@ -11,6 +11,24 @@ const method = {
     none: 255
 };
 
+const type = {
+    water: 1,
+    electricity: 11,
+    gas: 21,
+    heating: 31
+};
+
+const decodeType = (t) => {
+    const STRING = {
+        [type.water]: '水费',
+        [type.electricity]: '电费',
+        [type.gas]: '燃气费',
+        [type.heating]: '取暖费',
+    };
+
+    return STRING[t];
+};
+
 const methodDecoder = {
     [method.byOrganization]: "租客自主交纳",
     [method.byLord]: "由房东收取",
@@ -25,4 +43,6 @@ function decodeMethod(m) {
 module.exports = {
     method,
     decodeMethod,
+    type,
+    decodeType,
 };
